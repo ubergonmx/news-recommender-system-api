b896eab5eaab035e58addd72e803be28806779dc
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-10-26 04:52:07 +0800

    Add GoogleNews and `search` endpoint

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,7 @@
 import sys
 from flask import Flask, request, jsonify
 from newspaper import Article
+from GoogleNews import GoogleNews
 import logging
 
 # Configure the logging level and format
@@ -9,6 +10,9 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s",
     stream=sys.stdout,
 )
+
+# Configure the GoogleNews client
+googlenews = GoogleNews(lang='en')
 
 # Initialize the Flask application
 flask_app = Flask(__name__)
@@ -24,23 +28,71 @@ def value_error_handler(e):
 def feed():
     # Try-except block to handle errors
     try:
+        # Get feed from Google News
+        googlenews.get_news('recent news')
+
+        # Store results
+        results = clean_articles(googlenews.results())
+
+        # Clear GoogleNews results 
+        googlenews.clear()
+
+        # Return the feed as a JSON response
+        return jsonify(results)
+
         # Get the URL from the query parameters
-        url = request.args.get('url')
-        # Parse the URL using newspaper3k
-        article = Article(url)
-        article.download()
-        article.parse()
-        # Return the article as a JSON response
-        return jsonify({
-            'title': article.title,
-            'authors': article.authors,
-            'publish_date': article.publish_date,
-            'top_image': article.top_image,
-            'text': article.text,
-        })
+        # url = request.args.get('url')
+        # # Parse the URL using newspaper3k
+        # article = Article(url)
+        # article.download()
+        # article.parse()
+        # # Return the article as a JSON response
+        # return jsonify({
+        #     'title': article.title,
+        #     'authors': article.authors,
+        #     'publish_date': article.publish_date,
+        #     'top_image': article.top_image,
+        #     'text': article.text,
+        # })
     except Exception as e:
         logging.error(e)
         return jsonify(error=str(e)), 500
+    
+
+# Define a GET method for "search" endpoint
+@flask_app.route('/search', methods=['GET'])
+def search():
+    # Try-except block to handle errors
+    try:
+        # Get the query string from the query parameters
+        query = request.args.get('q')
+        # Get the top 5 results from Google News
+        googlenews.search(query)
+        result = clean_articles(googlenews.result())
+
+        # Clear GoogleNews results
+        googlenews.clear()
+
+        # Return the results as a JSON response
+        return jsonify(result[:5])
+    except Exception as e:
+        logging.error(e)
+        return jsonify(error=str(e)), 500
+    
+# Clean the articles returned by GoogleNews
+def clean_articles(articles):
+    # Add "body" key to each news article object
+    for article in articles:
+        # Parse the article using newspaper3k
+        logging.info("Parsing article %s", article['link'])
+        news_article = Article(article['link'])
+        news_article.download()
+        news_article.parse()
+
+        # Add the "body" key to the article object
+        article['body'] = news_article.text
+    
+    return articles
 
 # Run the app
 if __name__ == '__main__':
